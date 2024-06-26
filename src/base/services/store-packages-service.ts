7273d756c7e5b07a852d6aef1c3f44ffd3141a6a
EdoMathiasOW <edo.mathias@overwolf.com>
2024-06-26 15:22:31 +0300

    Experiment: Handle fetch for new categories endpoint, render categories and packages accordingly

--- src/base/services/store-packages-service.ts
+++ src/base/services/store-packages-service.ts
@@ -6,6 +6,10 @@ import { EventEmitter } from 'events';
 import ArraysIntersect from '../../utils/arrays-intersect';
 import OverwolfCategoriesRequest from '../../utils/overwolf-categories-request';
 import { AccountServiceBase, AccountToken } from './account-service';
+import {
+  RenderCategoryServiceBase,
+  RenderCategoryToken,
+} from './render-category-service';
 
 export type StorePackage = {
   base_price: number;
@@ -25,20 +29,48 @@ export type StorePackage = {
   updated_at: string;
 };
 
+export type StoreCategory = {
+  name: string;
+  description: string;
+  activeTier?: ActiveTier;
+  packages: StorePackage[];
+};
+
+export type ActiveTier = {
+  tierId: number;
+  package: StorePackage;
+};
+
+export type NewStorePackage = {
+  packageId: number;
+  name: string;
+  description: string;
+  totalPrice: number;
+  discount: number;
+  currency: string;
+};
+
 export const StorePackagesToken = 'StorePackagesBase';
 
 export type StorePackagesEvents = {
   updated: [StorePackage[]];
+  updatedCategory: [StoreCategory[]];
 };
 
 @injectable()
 // eslint-disable-next-line prettier/prettier
 export class StorePackagesServiceBase extends EventEmitter<StorePackagesEvents> {
-  private readonly listView;
+  // private readonly listView;
+  private readonly categoryView;
   private currentPackages: StorePackage[] = [];
+  private currentCategories: StoreCategory[] = [];
 
   public GetCurrentPackages() {
     return this.currentPackages;
+  }
+
+  public GetCurrentCategories() {
+    return this.currentCategories;
   }
 
   public async RefreshPackages(): Promise<boolean> {
@@ -79,6 +111,44 @@ export class StorePackagesServiceBase extends EventEmitter<StorePackagesEvents> 
     );
   }
 
+  public async RefreshCategories(): Promise<boolean> {
+    return this.accountService.GenerateToken().then(
+      async (token: string) => {
+        const headers = new Headers();
+        headers.append('Authorization', `Bearer ${token}`);
+        const requestUrl = OverwolfCategoriesRequest(endpoints.subscriptions);
+
+        return fetch(requestUrl, {
+          headers,
+        }).then((result) => {
+          if (result.status !== 200) {
+            console.error(
+              `Request failed! ${result.status} ${result.statusText}`,
+            );
+            throw new Error(
+              `Request failed! ${result.status} ${result.statusText}`,
+            );
+          }
+
+          return result.json().then(
+            (newCategories: StoreCategory[]) => {
+              console.log('new categories: ', newCategories);
+              return this.HandleNewCategories(newCategories);
+            },
+            (reason) => {
+              console.error('JSON parse error: ', reason);
+              throw new Error(`Request failed! ${reason}`);
+            },
+          );
+        });
+      },
+      (reason: string) => {
+        console.log(`Unable to generate token: ${reason}`);
+        return this.HandleNewCategories([]);
+      },
+    );
+  }
+
   private HandleNewPackages(newPackages: StorePackage[]) {
     const changed = !ArraysIntersect(newPackages, this.currentPackages);
 
@@ -93,6 +163,23 @@ export class StorePackagesServiceBase extends EventEmitter<StorePackagesEvents> 
     return changed;
   }
 
+  private HandleNewCategories(newCategories: StoreCategory[]) {
+    const changed = !ArraysIntersect(newCategories, this.currentCategories);
+
+    if (changed) {
+      this.currentCategories = newCategories;
+      this.HandleNewPackages(
+        newCategories.flatMap((category) => category.packages),
+      );
+      console.log('Categories Status!', this.currentCategories);
+      this.emit('updatedCategory', this.currentCategories);
+
+      this.Rerender();
+    }
+
+    return changed;
+  }
+
   public constructor(
     @inject(RenderListToken)
     renderListService: RenderListServiceBase,
@@ -100,35 +187,95 @@ export class StorePackagesServiceBase extends EventEmitter<StorePackagesEvents> 
     checkoutService: CheckoutServiceBase,
     @inject(AccountToken)
     private readonly accountService: AccountServiceBase,
+    @inject(RenderCategoryToken)
+    renderCategoryService: RenderCategoryServiceBase,
   ) {
     super();
-    this.listView = renderListService.CreateRenderer<StorePackage>((pack) => {
-      const container = document.createElement('li');
-      container.classList.add('vertical', 'item');
 
-      container.appendChild(renderListService.CreateText(pack.name));
-      container.appendChild(
-        renderListService.CreateText(pack.total_price.toString()),
-      );
+    this.categoryView = renderCategoryService.CreateRenderer<StoreCategory>(
+      (category) => {
+        const container = document.createElement('li');
+        container.classList.add('vertical', 'item');
 
-      const select = document.createElement('button');
-      select.textContent = 'checkout';
-      select.addEventListener('click', () => {
-        const discordId = document.getElementById(
-          'discordId',
-        ) as unknown as HTMLInputElement;
-        checkoutService.RequestCheckout({
-          packageId: pack.id,
-          extra: {
-            discordId: discordId.value ?? '',
-          },
+        const categoryTitle = renderCategoryService.CreateText(category.name);
+        categoryTitle.classList.add('category-title');
+
+        container.appendChild(categoryTitle);
+
+        const categoryList = document.createElement('ul');
+        categoryList.classList.add('horizontal', 'category-list');
+
+        category.packages.forEach((pack) => {
+          const categoryItem = document.createElement('li');
+          categoryItem.classList.add('vertical', 'item');
+
+          categoryItem.appendChild(renderCategoryService.CreateText(pack.name));
+
+          categoryItem.appendChild(
+            renderCategoryService.CreateText(pack.total_price.toString()),
+          );
+
+          const select = document.createElement('button');
+          select.textContent = 'checkout';
+          select.addEventListener('click', () => {
+            const discordId = document.getElementById(
+              'discordId',
+            ) as unknown as HTMLInputElement;
+            checkoutService.RequestCheckout({
+              packageId: pack.id,
+              extra: {
+                discordId: discordId.value ?? '',
+              },
+            });
+          });
+          categoryItem.appendChild(select);
+
+          categoryList.appendChild(categoryItem);
+
+          container.appendChild(categoryList);
         });
-      });
-      container.appendChild(select);
+        const hr = document.createElement('hr');
+        container.appendChild(hr);
 
-      return container;
-    }, document.getElementById('packages'));
+        return container;
+      },
+      document.getElementById('categories-container'),
+    );
+
+    // this.listView = renderListService.CreateRenderer<StorePackage>((pack) => {
+    //   console.log('pack: ', pack);
+
+    //   const container = document.createElement('li');
+    //   container.classList.add('vertical', 'item');
+
+    //   container.appendChild(renderListService.CreateText(pack.name));
+    //   // container.appendChild(
+    //   //   renderListService.CreateText(pack.total_price.toString()),
+    //   // );
+
+    //   const select = document.createElement('button');
+    //   select.textContent = 'checkout';
+    //   // select.addEventListener('click', () => {
+    //   //   const discordId = document.getElementById(
+    //   //     'discordId',
+    //   //   ) as unknown as HTMLInputElement;
+    //   //   checkoutService.RequestCheckout({
+    //   //     packageId: pack.id,
+    //   //     extra: {
+    //   //       discordId: discordId.value ?? '',
+    //   //     },
+    //   //   });
+    //   // });
+    //   container.appendChild(select);
+
+    //   console.log('container: ', container);
+
+    //   return container;
+    // }, document.getElementById('category-title'));
   }
 
-  public Rerender = () => this.listView.RefreshList(this.currentPackages);
+  public Rerender = () => {
+    this.categoryView.RefreshList(this.currentCategories);
+    // this.listView.RefreshList(this.currentPackages);
+  };
 }
